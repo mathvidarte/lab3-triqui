efeef63a320228436a205d228ca7508f09f38edc
natalia ortiz <nataliaortiz@MacBook-Air-de-natalia-3.local>
2020-02-14 11:07:18 -0500

    porfin pintan bien melasss yei

--- sketch.js
+++ sketch.js
@@ -1,36 +1,14 @@
-let filas = 3;
-let col = 3;
-
-/*let mapa = [
-    [0,0,0],
-    [0,0,0],
-    [0,0,0],
-]*/
-
-let mapa = [[]];
-
-let eo;
-let ayuwoki;
-
-let figuras = [];
-
+let mapa = new Array(3);
 let turno = 1;
 let cambiarTurno = false;
-
-
-function preload(){
-    eo = loadImage("images/eo.png");
-    ayuwoki = loadImage("images/hehe.jpg");
-}    
 
 function setup () {
     createCanvas (600,600);
 
-    mapa = [filas[col]];
-
-    for (let i = 0; i < filas; i++){
-        for(let j = 0; j < col; j++){
-            mapa[i[j]] = 0;
+    for (let i = 0; i < 3; i++){
+        mapa[i] = new Array(3);
+        for(let j = 0; j < 3; j++){
+            mapa[i][j] = new Espacio (i*200, j*200, 0);
         }
     }
 }
@@ -44,49 +22,35 @@ function draw () {
     line(0,200,600,200);
     line(0,400,600,400);
 
-    for (let i = 0; i < figuras.length; i++) {
-        figuras[i].pintar();
-    }
-
-    for (let i = 0; i < filas; i++){
-        for(let j = 0; j < col; j++){
-            switch(mapa[i[j]]){
-                case 0:
-                    //nada
-                    break;
-                case 1:
-                    //circulo
-                    figuras.push(new circulo(i*200, j*200));
-                    break;
-                case 2:
-                    //equis
-                    figuras.push(new equis (i*200, j*200));
-                    break;
-            }
+    for (let i = 0; i < 3; i++){
+        for(let j = 0; j < 3; j++){
+            mapa[i][j].pintar();
         }
-    }
-       
+    }  
 }
 
 function mousePressed() {
 
-    for (let i = 0; i < filas; i++){
-        for(let j = 0; j < col; j++){
+    for (let i = 0; i < 3; i++){
+        for(let j = 0; j < 3; j++){
             if(mouseX > i*200 && mouseX < i*200 + 200 && mouseY > j*200 && mouseY < j*200+200){
-                if(mapa[i[j]] == 0){
-                    mapa[i[j]] = turno;
+                if(mapa[i][j].num == 0){
 
-                    if(!cambiarTurno){
-                        turno = 2;
-                        cambiarTurno = true;
-                        return;
-                    }
-                    if(cambiarTurno){
+                    if (cambiarTurno == false){
                         turno = 1;
+                        cambiarTurno = true;
+                    }else{
+                        turno = 2;
                         cambiarTurno = false;
                     }
+
+                    mapa[i][j].num = turno;
+                    mapa[i][j].px = i*200;
+                    mapa[i][j].py = j*200;
+
+                    console.log(mapa[i][j])
+                    }    
                 }
             }
         }
     }
-}
